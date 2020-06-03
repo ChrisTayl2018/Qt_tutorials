3ae626a5290f25fcdecfb984c67bcdbccaf6a25d
ChrisTayl2018 <ctaylor1991@live.com.au>
2020-06-03 12:15:40 +1000

    Commit num 2: Different methods of constructing and decosntructing the shared pointer This includes the assignment solution  Changes to be committed: 	modified:   QT_Core_Intermediate/QTAssignment1/main.cpp 	modified:   QT_Core_Intermediate/QTAssignment1/test.cpp 	modified:   QT_Core_Intermediate/QTAssignment1/test.h

--- QT_Core_Intermediate/QTAssignment1/main.cpp
+++ QT_Core_Intermediate/QTAssignment1/main.cpp
@@ -1,10 +1,29 @@
 #include <QCoreApplication>
+#include <QDebug>
+#include <QScopedPointer>
+#include "test.h"
+
+void assignmenTSolution_doStuff(){
+    QScopedPointer<test> test2(new test());
+    qInfo() << test2.data();
+    // differences here being it deconstructs the test class when at the end of the scope
+}
+
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
+    // Construct the object
+    QScopedPointer<test> pointer1(new test()) ;
+    // If object exists
+    if(pointer1){qInfo() << pointer1.data();} //if pointer exists
+    // call the deconstructor
+    pointer1->deleteLater();
 
+
+    // Assignment solution execution:
+    assignmenTSolution_doStuff();
 
     return a.exec();
 }
--- QT_Core_Intermediate/QTAssignment1/test.cpp
+++ QT_Core_Intermediate/QTAssignment1/test.cpp
@@ -1,6 +1,12 @@
 #include "test.h"
+#include <QDebug>
 
 test::test(QObject *parent) : QObject(parent)
 {
+    qInfo() << "Constructed object" << this;
+}
 
+test::~test()
+{
+    qInfo() << "Deconstructed object" << this;
 }
